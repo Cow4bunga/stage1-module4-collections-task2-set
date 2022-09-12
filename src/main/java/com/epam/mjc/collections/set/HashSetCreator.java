e8d854b74ca64e8dfd264c4b32415ba565a8ff06
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-12 19:52:30 +0300

    done(fixed 1)

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,27 +1,25 @@
 package com.epam.mjc.collections.set;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
-        HashSet<Integer> hSet = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         for (Integer item : sourceList) {
             if (item % 2 == 0) {
-                int elem = item;
-                hSet.add(elem);
-                while (elem % 2 != 1 && elem != 0) {
-                    hSet.add(elem);
-                    elem /= 2;
+                do {
+                    set.add(item);
+                    item /= 2;
                 }
+                while (item % 2 == 0);
+                set.add(item);
             } else {
-                hSet.add(item);
-                hSet.add(item * 2);
+                set.add(item);
+                set.add(item * 2);
             }
 
         }
-        return hSet;
+        return set;
     }
 }
